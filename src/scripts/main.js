a7ec1d3f9ecb3b697425d7d7db57a4101b629c8e
Zolkiewskyi Oleg <zolkiewskyioleg@gmail.com>
2022-07-27 15:53:00 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write your code here
+const urlList
+= 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+const urlDetails
+= 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+
+const getPhones = () => {
+  return fetch(urlList)
+    .then(response => {
+      if (!response.ok
+        && response.headers.get('content-type').includes('application/json')) {
+        return Promise.reject(new Error('Error!!!'));
+      }
+
+      return response.json();
+    });
+};
+
+function createList(phonesList) {
+  const ul = document.createElement('ul');
+
+  document.querySelector('body').append(ul);
+
+  return phonesList.map(phone => {
+    const li = document.createElement('li');
+
+    li.innerText = phone.name;
+    ul.append(li);
+
+    return phone.id;
+  });
+}
+
+function getPhonesDetails(id) {
+  const urls = id.map(phone => `${urlDetails}${phone.id}.json`);
+
+  return Promise.all(urls);
+};
+
+getPhones()
+  .then(createList)
+  .then(getPhonesDetails)
+  .catch(error => setTimeout(() => error, 5000));
